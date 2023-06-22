289a45d1c8489608c848771a945016556ad87091
Sarah Raguin <sarahraguin@gmail.com>
2023-06-22 11:11:38 -0400

    Initialization of React Router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path='/' element={<App />} />
+    </Router>
   </React.StrictMode>
-);
+
+)
+document.getElementById('root');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
