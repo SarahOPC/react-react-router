ac965e128b2f9618e0db10931b376adb8d615220
Sarah Raguin <sarahraguin@gmail.com>
2023-07-11 10:02:49 -0400

    Working on hooks

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import HomeBodyThumb from "./HomeBodyThumb";
 import styled from 'styled-components';
+import { useState, useEffect } from "react";
 
 const HomeBodyContainer = styled.div`
     background-color: #F6F6F6;
@@ -13,6 +14,24 @@ const HomeBodyContainer = styled.div`
 `
 
 function HomeBody() {
+
+    const [dataContent, setDataContent] = useState();
+
+    useEffect(() => {
+        // fetch data
+        const dataFetch = async () => {
+            const dataContent = await (
+                await fetch(
+                "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json"
+                )
+            ).json();
+
+        // set state when the data received
+        setDataContent(dataContent);
+        };
+
+        dataFetch();
+    }, []);
 
     return (
         <HomeBodyContainer>
