8f23958f11dbdcb4da968fd8c449b2c1d2d3b31c
Sarah Raguin <sarahraguin@gmail.com>
2023-07-24 08:38:42 -0400

    Solving a warning on the console

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -2,7 +2,12 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+@font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 100 900;
+    src: url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  }
 
 body {
     font-family: 'Montserrat', sans-serif;
